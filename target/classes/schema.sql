4572c332e2515728daa8ad1e8214a2359ea8d737
Your Name <you@example.com>
2020-01-12 18:18:25 +0900

    Flyway導入

--- target/classes/schema.sql
+++ target/classes/schema.sql
@@ -1,1 +1,1 @@
-CREATE table customers(id int primary key auto_increment, first_name varchar(30), last_name varchar(30));
+CREATE TABLE IF NOT EXISTS customers(id int primary key auto_increment, first_name varchar(30), last_name varchar(30));
